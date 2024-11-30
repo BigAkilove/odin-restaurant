d0eba633d5626a95714b0f0497dd82da9d5d8163
Valou <anonanonymous018@gmail.com>
2024-11-30 13:03:56 +0100

    add html creating logic in home.js and create tabs to change content

---
+++ src/home.js
@@ -0,0 +1,32 @@
+export function createHomePage() {
+    const content = document.querySelector('#content')
+
+    const descriptionAndImagesContainer = document.createElement('div');
+    descriptionAndImagesContainer.classList.add('description-and-images');
+
+    content.appendChild(descriptionAndImagesContainer);
+
+    const paragraph = document.createElement('p');
+    const paragraphTextNode = document.createTextNode("Hello, this is my epic restaurant");
+
+    paragraph.appendChild(paragraphTextNode);
+    descriptionAndImagesContainer.appendChild(paragraph)
+
+    const imageContainer = document.createElement('div');
+    imageContainer.classList.add('example-images-container');
+
+    const exampleImage = document.createElement('div');
+    exampleImage.classList.add('example-image');
+
+    descriptionAndImagesContainer.appendChild(imageContainer);
+
+    for(let i=0;i<4;i++) imageContainer.appendChild(exampleImage.cloneNode());
+
+    const quoteParagraph = document.createElement('div');
+    const quoteParagraphTextNode = document.createTextNode('This restaurant was created in 2024 and already received 30 recommendations from world famous chiefs.')
+    quoteParagraph.classList.add('restaurant-quote');
+    quoteParagraph.appendChild(quoteParagraphTextNode);
+
+    content.appendChild(quoteParagraph);
+}
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './styles.css'
+import { createHomePage } from './home'
+
+createHomePage();
+
+document.addEventListener('click', (event) => {
+    console.log(event.target.innerText);
+    const content = document.querySelector('#content')
+    content.innerHTML= ''
+    switch(event.target.innerText) {
+        case('Home') :
+            console.log(1);
+            createHomePage();
+            break;
+        case('Menu') :
+            console.log(2);
+            break;
+        case('About') :
+            console.log(3);
+    }
+})
